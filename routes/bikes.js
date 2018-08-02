16b677581b0d2cfcd0d62bb870cfd3105531ae83
ken woodruff <kwoodruf@adobe.com>
2018-08-01 21:42:54 -0700

    Added bike and bikes pages with link from latter to former for each bike

--- routes/bikes.js
+++ routes/bikes.js
@@ -5,8 +5,14 @@ var bikes = require('../lib/bikes.js');
 /* GET bikes page. */
 router.get('/', async function(req, res, next) {
   var deedid = req.query.deedid;
-  res.render('bikes', { title: 'BikeDeed', bike: await bikes.getBike(deedid),
-    pageTestScript: '/qa/tests-bikes.js' });
+  if (deedid == '' || deedid === undefined) {
+    res.render('bikes', { bikes: await bikes.getBikes(),
+      pageTestScript: '/qa/tests-bikes.js' });
+  }
+  else {
+    res.render('bike', { bike: await bikes.getBike(deedid),
+      pageTestScript: '/qa/tests-bikes.js' });
+  }
 });
 
 module.exports = router;
